8f6118a1f56f9ff6a6478665e6dddd6abd529fd4
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2017-06-04 21:11:15 +0100

    chain now has samples and weights

--- bclest/mc/chain.h
+++ bclest/mc/chain.h
@@ -5,21 +5,26 @@
 #include <stdexcept>
 #include <type_traits>
 
+#include <Eigen/Dense>
+
 namespace bclest {
 namespace mc {
 
 template <class RealScalar> class Chain {
   static_assert(std::is_floating_point<RealScalar>::value,
                 "Chain should be a floating point type");
+  typedef Eigen::Matrix<RealScalar, Eigen::Dynamic, Eigen::Dynamic> RealMatrix;
+  typedef Eigen::Matrix<RealScalar, Eigen::Dynamic, 1> RealVector;
 
 public:
   explicit Chain(std::size_t const num_samples, std::size_t const num_dims)
-      : num_samples_(num_samples), num_dims_(num_dims) {
+      : num_samples_(num_samples), num_dims_(num_dims),
+        samples_(num_samples, num_dims), weights_(num_samples) {
     if (num_samples == std::size_t(0)) {
-      throw std::logic_error("num_samples should be > 0");
+      throw std::length_error("num_samples should be > 0");
     }
     if (num_dims == std::size_t(0)) {
-      throw std::logic_error("num_dims should be > 0");
+      throw std::length_error("num_dims should be > 0");
     }
   }
 
@@ -29,9 +34,27 @@ public:
 
   inline std::size_t NumDims() const { return num_dims_; }
 
+  inline RealMatrix const &GetSamples() const { return samples_; }
+
+  inline RealVector const &GetWeights() const { return weights_; }
+
+  void SetSample(std::size_t const sample_id, RealVector const &sample,
+                 RealScalar const weight) {
+    if (sample_id >= num_samples_) {
+      throw std::length_error("sample_id should be < num_samples_");
+    }
+    if (std::size_t(sample.rows()) != num_dims_) {
+      throw std::length_error("rows in samples should be = num_dims_");
+    }
+    samples_.row(sample_id) = sample;
+    weights_(sample_id) = weight;
+  }
+
 private:
   std::size_t num_samples_;
   std::size_t num_dims_;
+  RealMatrix samples_;
+  RealVector weights_;
 };
 
 } // end namespace bclest
--- tests/mc/chain_test.cc
+++ tests/mc/chain_test.cc
@@ -1,5 +1,8 @@
 #include <bclest/mc/chain.h>
 #include <gtest/gtest.h>
+#include <stdexcept>
+
+#include <Eigen/Dense>
 
 TEST(ChainTest, ChainConstructor) {
   using namespace bclest::mc;
@@ -8,4 +11,46 @@ TEST(ChainTest, ChainConstructor) {
   Chain<double> chain(num_samples, num_dims);
   ASSERT_EQ(chain.NumSamples(), num_samples);
   ASSERT_EQ(chain.NumDims(), num_dims);
+}
+
+TEST(ChainTest, ChainConstructorNumSamplesZero) {
+  using namespace bclest::mc;
+  std::size_t const num_samples(0);
+  std::size_t const num_dims(100);
+  try {
+    Chain<double> chain(num_samples, num_dims);
+    // if the line below executes, the test should fail.
+    // as we expect an exception to be thrown
+    ASSERT_NE(chain.NumSamples(), num_samples);
+  } catch (std::logic_error &lgerr) {
+  }
+}
+
+TEST(ChainTest, ChainConstructorNumDimesZero) {
+  using namespace bclest::mc;
+  std::size_t const num_samples(1000);
+  std::size_t const num_dims(0);
+  try {
+    Chain<double> chain(num_samples, num_dims);
+    // if the line below executes, the test should fail.
+    // as we expect an exception to be thrown
+    ASSERT_NE(chain.NumDims(), num_dims);
+  } catch (std::length_error &lgerr) {
+  }
+}
+
+TEST(ChainTest, ChainSetSample) {
+  using namespace bclest::mc;
+  using namespace Eigen;
+  std::size_t const num_samples(1000);
+  std::size_t const num_dims(10);
+  Chain<double> chain(num_samples, num_dims);
+  VectorXd const sample = VectorXd::Random(num_dims);
+  std::size_t const sample_id(10);
+  double const weight(0.5);
+  chain.SetSample(sample_id, sample, weight);
+  for (std::size_t i = 0; i < num_dims; ++i) {
+    ASSERT_EQ(chain.GetSamples().row(sample_id)(i), sample(i));
+  }
+  ASSERT_EQ(chain.GetWeights()(sample_id), weight);
 }
